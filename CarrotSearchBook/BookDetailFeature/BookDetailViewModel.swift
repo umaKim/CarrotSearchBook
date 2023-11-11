accfcde7b2042f0855050aa6e50f3639490469df
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-11-12 03:52:55 +0900

    [CLEAN] BookDetailDomain 코드를 삭제

--- CarrotSearchBook/BookDetailFeature/BookDetailViewModel.swift
+++ CarrotSearchBook/BookDetailFeature/BookDetailViewModel.swift
@@ -14,23 +14,6 @@ enum BookDetailViewModelListenerType {
     case message(String, String)
 }
 
-struct BookDetailDomain {
-    let title: String?
-    let subtitle: String?
-    let authors: String?
-    let publisher: String?
-    let language: String?
-    let isbn10: String?
-    let isbn13: String?
-    let pages: String?
-    let year: String?
-    let rating: String?
-    let desc: String?
-    let price: String?
-    let image: String?
-    let url: String?
-}
-
 final class BookDetailViewModel {
     private(set) lazy var transitionPublisher   = transitionSubject.eraseToAnyPublisher()
     private let transitionSubject               = PassthroughSubject<BookDetailTransition, Never>()
