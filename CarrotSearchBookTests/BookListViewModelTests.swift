6fdf1d0c68cad717dddba64aeab298bea30ad729
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-11-12 04:24:52 +0900

    [TEST] BookListViewModelTests 추가
    - testFetchBooksSuccess
    - testFetchBooksEmpty
    - testFetchBooksError
    - viewModel test를 위한 MockBookListRepository 추가

---
+++ CarrotSearchBookTests/BookListViewModelTests.swift
@@ -0,0 +1,79 @@
+//
+//  BookListViewModelTests.swift
+//  BookListViewModelTests
+//
+//  Created by 김윤석 on 2023/11/12.
+//
+
+import Combine
+import XCTest
+@testable import CarrotSearchBook
+
+class BookListViewModelTests: XCTestCase {
+    var viewModel: BookListViewModel!
+    var mockRepository: MockBookListRepository!
+    var cancellables: Set<AnyCancellable>!
+
+    override func setUp() {
+        super.setUp()
+        mockRepository = MockBookListRepository()
+        viewModel = BookListViewModel(mockRepository)
+        cancellables = []
+    }
+
+    override func tearDown() {
+        viewModel = nil
+        mockRepository = nil
+        cancellables = nil
+        super.tearDown()
+    }
+
+    func testFetchBooksSuccess() {
+        let expectation = XCTestExpectation(description: "Successfully fetched books")
+
+        viewModel.listenPublisher.sink { [weak self] listenerType in
+            if case .update = listenerType {
+                print(self?.viewModel.books)
+                XCTAssertTrue(self?.viewModel.books.count == 1, "Books count should be 1")
+                expectation.fulfill()
+            }
+        }.store(in: &cancellables)
+
+        viewModel.updateQuery("Test", completion: {})
+        wait(for: [expectation], timeout: 500.0)
+    }
+
+    func testFetchBooksEmpty() {
+        mockRepository.shouldReturnEmpty = true
+        let expectation = XCTestExpectation(description: "No books fetched")
+
+        viewModel
+            .listenPublisher
+            .sink { [weak self] listenerType in
+            if case .update = listenerType {
+                XCTAssertTrue(self?.viewModel.books.isEmpty == true, "Books array should be empty")
+                expectation.fulfill()
+            }
+        }.store(in: &cancellables)
+
+        viewModel.updateQuery("Test", completion: {})
+        wait(for: [expectation], timeout: 5.0)
+    }
+
+    func testFetchBooksError() {
+        mockRepository.shouldReturnError = true
+        let expectation = XCTestExpectation(description: "Error in fetching books")
+
+        viewModel
+            .listenPublisher
+            .sink { listenerType in
+            if case .message(let title, _) = listenerType {
+                XCTAssertEqual(title, "Error", "Error message should be triggered")
+                expectation.fulfill()
+            }
+        }.store(in: &cancellables)
+
+        viewModel.updateQuery("Test", completion: {})
+        wait(for: [expectation], timeout: 5.0)
+    }
+}
---
+++ CarrotSearchBookTests/Mocks/MockBookListRepository.swift
@@ -0,0 +1,35 @@
+//
+//  MockBookListRepository.swift
+//  CarrotSearchBookTests
+//
+//  Created by 김윤석 on 2023/11/12.
+//
+
+import Foundation
+@testable import CarrotSearchBook
+
+final class MockBookListRepository: BookListRepository {
+    var shouldReturnEmpty = false
+    var shouldReturnError = false
+    var mockBooks: [BookDomain] = [
+        .init(id: UUID(), title: "test", subtitle: "test", isbn13: "123", price: "3.00", image: "", url: "test.com")
+    ]
+
+    func fetchBooks(for title: String, page: Int) async throws -> BookResponseDomain {
+        if shouldReturnError {
+            throw NSError(domain: "TestError", code: 0, userInfo: nil)
+        }
+
+        if shouldReturnEmpty {
+            return BookResponseDomain(total: "0", page: "1", books: [])
+        } else {
+            // Return a predefined set of books or dynamically generate them based on the needs of your test
+            return BookResponseDomain(total: "\(mockBooks.count)", page: "\(page)", books: mockBooks)
+        }
+    }
+
+    // Helper method to set mock books data
+    func setMockBooks(_ books: [BookDomain]) {
+        self.mockBooks = books
+    }
+}
