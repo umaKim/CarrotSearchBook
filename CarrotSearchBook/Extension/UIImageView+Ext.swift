11d8f4f11a97fa291bb61c1b06399ce21fe4cde2
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-11-13 10:21:42 +0900

    [REFACTOR] retain cycle 방지

--- CarrotSearchBook/Extension/UIImageView+Ext.swift
+++ CarrotSearchBook/Extension/UIImageView+Ext.swift
@@ -27,14 +27,14 @@ extension UIImageView {
     
     private func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
         contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
             guard
                 let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
                 let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
                 let data = data, error == nil,
                 let image = UIImage(data: data)
             else { return }
-            self.cacheImageSetter(with: url.absoluteString, and: image)
+            self?.cacheImageSetter(with: url.absoluteString, and: image)
             DispatchQueue.main.async() { [weak self] in
                 self?.image = image
             }
